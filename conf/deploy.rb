c842f58d2495c5c987726b27a18179689981c76d
Trevor Burton <worldofpaper@googlemail.com>
2012-11-16 16:03:12 +0000

    added deploy file and crossdomain.xml and the route for crossdomain to be picked up

--- conf/deploy.rb
+++ conf/deploy.rb
@@ -1,13 +1,13 @@
 ssh_options[:forward_agent] = true
 set :application, "ogilvy-interviews"
 set :repository,  "ssh://git@github.com/quicktv/#{application}.git"
-set :branch, 'bug_fixes'
+set :branch, 'master'
 set :scm, :git
 set :play_path, "/usr/local/play-current"
 set :shared_path, "#{deploy_to}/shared"
 set :app_pid, "#{shared_path}/pids/server.pid"
 set :app_path, "#{deploy_to}/current"
-set :domain, "176.34.116.43"
+set :domain, "54.246.105.99"
 set :user, "ubuntu"
 set :svn_user, ENV['svn_user']
 set :svn_password, ENV['svn_password']
